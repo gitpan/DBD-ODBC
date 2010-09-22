5790fec2ad6be394c3a9bcc17375483003c8817a
Martin J Evans <mjevans@cpan.org>
2010-09-22 07:46:46 +0000

    Import of MJEVANS/DBD-ODBC-1.25 from CPAN.
    gitpan-cpan-distribution: DBD-ODBC
    gitpan-cpan-version:      1.25
    gitpan-cpan-path:         MJEVANS/DBD-ODBC-1.25.tar.gz
    gitpan-cpan-author:       MJEVANS
    gitpan-cpan-maturity:     released

--- ODBC.pm
+++ ODBC.pm
@@ -1,4 +1,4 @@
-# $Id: ODBC.pm 14418 2010-09-17 13:23:25Z mjevans $
+# $Id: ODBC.pm 14442 2010-09-22 07:40:40Z mjevans $
 #
 # Copyright (c) 1994,1995,1996,1998  Tim Bunce
 # portions Copyright (c) 1997-2004  Jeff Urlwin
@@ -19,7 +19,7 @@ require 5.006;
 # see discussion on dbi-users at
 # http://www.nntp.perl.org/group/perl.dbi.dev/2010/07/msg6096.html and
 # http://www.dagolden.com/index.php/369/version-numbers-should-be-boring/
-$DBD::ODBC::VERSION = '1.24_6';
+$DBD::ODBC::VERSION = '1.25';
 
 {
     ## no critic (ProhibitMagicNumbers ProhibitExplicitISA)
@@ -32,7 +32,7 @@ $DBD::ODBC::VERSION = '1.24_6';
 
     @ISA = qw(Exporter DynaLoader);
 
-    # my $Revision = substr(q$Id: ODBC.pm 14418 2010-09-17 13:23:25Z mjevans $, 13,2);
+    # my $Revision = substr(q$Id: ODBC.pm 14442 2010-09-22 07:40:40Z mjevans $, 13,2);
 
     require_version DBI 1.21;
 
@@ -526,7 +526,7 @@ DBD::ODBC - ODBC Driver for DBI
 
 =head1 VERSION
 
-This documentation refers to DBD::ODBC version 1.24_6.
+This documentation refers to DBD::ODBC version 1.25.
 
 =head1 SYNOPSIS
 
