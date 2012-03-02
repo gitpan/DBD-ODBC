7e3a16e06492db586903b12af3595501a24cac8a
Martin J Evans <mjevans@cpan.org>
2012-03-02 11:09:43 +0000

    Import of MJEVANS/DBD-ODBC-1.34_7 from CPAN.
    gitpan-cpan-distribution: DBD-ODBC
    gitpan-cpan-version:      1.34_7
    gitpan-cpan-path:         MJEVANS/DBD-ODBC-1.34_7.tar.gz
    gitpan-cpan-author:       MJEVANS
    gitpan-cpan-maturity:     developer

--- ODBC.pm
+++ ODBC.pm
@@ -1,4 +1,4 @@
-# $Id: ODBC.pm 15160 2012-02-14 21:06:54Z mjevans $
+# $Id: ODBC.pm 15186 2012-02-27 09:09:09Z mjevans $
 #
 # Copyright (c) 1994,1995,1996,1998  Tim Bunce
 # portions Copyright (c) 1997-2004  Jeff Urlwin
@@ -19,7 +19,7 @@ require 5.008;
 # see discussion on dbi-users at
 # http://www.nntp.perl.org/group/perl.dbi.dev/2010/07/msg6096.html and
 # http://www.dagolden.com/index.php/369/version-numbers-should-be-boring/
-$DBD::ODBC::VERSION = '1.34_6';
+$DBD::ODBC::VERSION = '1.34_7';
 
 {
     ## no critic (ProhibitMagicNumbers ProhibitExplicitISA)
@@ -32,7 +32,7 @@ $DBD::ODBC::VERSION = '1.34_6';
 
     @ISA = qw(Exporter DynaLoader);
 
-    # my $Revision = substr(q$Id: ODBC.pm 15160 2012-02-14 21:06:54Z mjevans $, 13,2);
+    # my $Revision = substr(q$Id: ODBC.pm 15186 2012-02-27 09:09:09Z mjevans $, 13,2);
 
     require_version DBI 1.609;
 
@@ -630,7 +630,7 @@ DBD::ODBC - ODBC Driver for DBI
 
 =head1 VERSION
 
-This documentation refers to DBD::ODBC version 1.34_6.
+This documentation refers to DBD::ODBC version 1.34_7.
 
 =head1 SYNOPSIS
 
--- dbdimp.c
+++ dbdimp.c
@@ -1,4 +1,4 @@
-/* $Id: dbdimp.c 15166 2012-02-16 21:01:43Z mjevans $
+/* $Id: dbdimp.c 15186 2012-02-27 09:09:09Z mjevans $
  *
  * portions Copyright (c) 1994,1995,1996,1997  Tim Bunce
  * portions Copyright (c) 1997 Thomas K. Wenrich
@@ -6780,11 +6780,12 @@ IV odbc_st_execute_for_fetch(
 
         for (p = 1; p <= n_params; p++) {
             char name[32];
-            sprintf(name, "%u", p);
             SV **phs_svp;
             phs_t *phs;
             STRLEN sv_len;
             char *sv_val;
+
+            sprintf(name, "%u", p);
 
             phs_svp = hv_fetch(imp_sth->all_params_hv, name, strlen(name), 0);
             if (phs_svp == NULL) {
